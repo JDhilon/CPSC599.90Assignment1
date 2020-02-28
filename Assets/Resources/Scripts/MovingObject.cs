12cdb353e27949c32a6425924ffd899f7d20459f
Jashanjot Dhilon <jashanjotdhilon@gmail.com>
2020-02-28 10:01:36 -0700

    Object moving between set points
    Object moving between set points

---
+++ Assets/Resources/Scripts/MovingObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] targets;
+
+    int currentPoint = 0;
+    float speed = 0.1f;
+    bool complete;
+
+    public bool moving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hi Terrance!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            // While it hasn't hit all 3 points
+            if (!complete)
+            {
+                Transform target = targets[currentPoint].transform;
+                float step = speed * Time.deltaTime; // calculate distance to move
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+                // Check if the position of the cube and capsule are approximately equal.
+                if (Vector3.Distance(transform.position, target.position) < 0.001f)
+                {
+                    // Move the position of the capsule.
+                    currentPoint++;
+                }
+            }
+            if (currentPoint >= targets.Length)
+            {
+                this.gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+                complete = true;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Resources/Scripts/TrackerTriggerUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerTriggerUpdate : DefaultTrackableEventHandler
+{
+    [SerializeField]
+    MovingObject movingObj;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        movingObj.moving = true;
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        movingObj.moving = false;
+    }
+}
